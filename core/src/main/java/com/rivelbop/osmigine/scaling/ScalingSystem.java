fab70d695cf3fdf8b27b143b86325539bb2df21b
RivelBop <rivelbop@gmail.com>
2026-01-08 22:50:18 -0500

    Added lastWidth and lastHeight to ScalingSystem
    To avoid calling Gdx.graphics.getWidth/Height constantly.

--- core/src/main/java/com/rivelbop/osmigine/scaling/ScalingSystem.java
+++ core/src/main/java/com/rivelbop/osmigine/scaling/ScalingSystem.java
@@ -8,18 +8,26 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 public final class ScalingSystem {
     private final Viewport viewport = new ScreenViewport();
 
-    private float targetWidth;
-    private float targetHeight;
+    private int lastWidth;
+    private int lastHeight;
+
+    private int targetWidth;
+    private int targetHeight;
     private float targetAspectRatio;
 
     private float currentAspectRatio;
     private float scale;
 
     public ScalingSystem(int targetWidth, int targetHeight) {
+        lastWidth = Gdx.graphics.getWidth();
+        lastHeight = Gdx.graphics.getHeight();
         setTargetSize(targetWidth, targetHeight);
     }
 
     public void resize(int width, int height) {
+        lastWidth = width;
+        lastHeight = height;
+
         viewport.update(width, height, true);
         currentAspectRatio = (float) width / height;
         scale = Math.min((float) width / targetWidth, (float) height / targetHeight);
@@ -33,18 +41,18 @@ public final class ScalingSystem {
         this.targetWidth = targetWidth;
         this.targetHeight = targetHeight;
         this.targetAspectRatio = (float) targetWidth / targetHeight;
-        resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        resize(lastWidth, lastHeight);
     }
 
     public Matrix4 combined() {
         return viewport.getCamera().combined;
     }
 
-    public float getTargetWidth() {
+    public int getTargetWidth() {
         return targetWidth;
     }
 
-    public float getTargetHeight() {
+    public int getTargetHeight() {
         return targetHeight;
     }
 
@@ -52,11 +60,11 @@ public final class ScalingSystem {
         return targetAspectRatio;
     }
 
-    public float getCurrentWidth() {
+    public int getCurrentWidth() {
         return viewport.getScreenWidth();
     }
 
-    public float getCurrentHeight() {
+    public int getCurrentHeight() {
         return viewport.getScreenHeight();
     }
 
