0e35409c24fe5ceeebe320fbd51c92d83edc60a5
RivelBop <djerzak@proton.me>
2026-05-11 02:09:54 -0400

    Set Vsync to False (Fixes MacOS Stutter)

--- lwjgl3/src/main/java/com/rivelbop/osmigine/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/rivelbop/osmigine/lwjgl3/Lwjgl3Launcher.java
@@ -25,7 +25,7 @@ public class Lwjgl3Launcher {
         configuration.setTitle("Osmigine");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
+        configuration.useVsync(false);
         //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
         //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
         //// NOTE: Can cause stutters on MacOS (so plus 1 is removed when using it)
